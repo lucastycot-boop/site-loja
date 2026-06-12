aacfc6a3cdff3d3c71e0816ca833059beabdb56e
lucastycot-boop <lucastycot@gmail.com>
2026-06-12 05:33:51 -0300

    Update main.py

--- main.py
+++ main.py
@@ -279,10 +279,10 @@ def deletar_produto(id):
     flash(f'Produto "{produto.nome}" removido completamente!', 'success')
     return redirect(url_for('painel_admin'))
 
-if __name__ == "__main__":
-    with app.app_context():
-        db.create_all()
+with app.app_context():
+    db.create_all()
 
+if __name__ == "__main__":
     # Configuração de porta dinâmica para servidores de hospedagem
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port, debug=True)
